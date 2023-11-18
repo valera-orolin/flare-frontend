60205080a1aa9a82361a96c3637e635d7a3abbcc
Valeryia Matveyeva <matveyeva.valeryia@gmail.com>
2023-11-18 14:51:54 +0300

    Add Profile page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var links = document.querySelectorAll('.nav-link'); // TODO объединить эти части
+/* TODO This code is not being executed. When working with the backend, toggling the clicked class for .nav-link needs to be provided. */
+var links = document.querySelectorAll('.nav-link');
 links.forEach(function(link) {
   link.addEventListener('click', function(event) {
-    event.preventDefault();
     links.forEach(function(otherLink) {
       if (otherLink !== link) {
         otherLink.classList.remove('clicked');
@@ -26,11 +26,11 @@ feedsChoice.forEach(function(link) {
 
 autosize(document.querySelector('#post-area'));
 
-document.getElementById('post-media-icon').addEventListener('click', function() {
+document.getElementById('post-media-icon')?.addEventListener('click', function() {
     document.getElementById('post-media').click();
 });
 
-document.getElementById('visibility-select').addEventListener('change', function(event) {
+document.getElementById('visibility-select')?.addEventListener('change', function(event) {
     if (event.target.value === 'public') {
         document.getElementById('globe-icon').classList.remove('hidden');
         document.getElementById('users-icon').classList.add('hidden');
@@ -42,11 +42,11 @@ document.getElementById('visibility-select').addEventListener('change', function
 
 const picker = document.querySelector('emoji-picker')
 const textarea = document.querySelector('#post-area')
-picker.addEventListener('emoji-click', event => {
+picker?.addEventListener('emoji-click', event => {
     textarea.value += event.detail.unicode
 })
 
-document.getElementById('emoji-icon').addEventListener('click', function() {
+document.getElementById('emoji-icon')?.addEventListener('click', function() {
     const emojiPicker = document.getElementById('emoji-picker');
     emojiPicker.classList.toggle('hidden');
 });
